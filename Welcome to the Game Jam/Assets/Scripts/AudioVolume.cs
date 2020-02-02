700353bd207cc3a6776a59bfd5be46c015f750e4
Crash Spasticoot <Joshua2.Beard@live.uwe.ac.uk>
2020-02-02 15:25:30 +0000

    Menu Fixed
    + Volume slider has use (but needs linking with all audio sources)
    + Added controller support

---
+++ Welcome to the Game Jam/Assets/Scripts/AudioVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolume : MonoBehaviour
+{
+    private AudioSource audio_source;
+    private float vol = 1.0f;
+
+    void Start()
+    {
+        audio_source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audio_source.volume = vol;
+    }
+
+    public void SetVolume(float volume)
+    {
+        vol = volume;
+    }
+}
